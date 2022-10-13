8a704d81b57a60cc32d4daceb86f5ee6790b258c
uselessgoddess <dodickgod@gmail.com>
2022-10-13 09:39:01 +0000

    Add link requesting from vsu.by

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use crate::faculties::Spec;
 use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use futures::StreamExt;
 use ron::Value;
+use scraper::{Html, Selector};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs;
@@ -22,16 +22,30 @@ type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
 const PRESET: &str = "presets/preset.ron";
 
+fn schedule_link(document: &str) -> Vec<String> {
+    let selector = Selector::parse(".table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(1) > p > a").unwrap();
+    let document = Html::parse_document(document);
+
+    document.select(&selector).map(|x| x.value().attr("href").unwrap().to_string()).collect()
+}
+
+async fn document(url: &str) -> Result<String> {
+    Ok(reqwest::get(url).await?.text().await?)
+}
+
+async fn do_schedule(info: &Info) {
+    let page = format!("https://vsu.by/universitet/fakultety/{}/raspisanie.html", info.link);
+    let document = document(&page).await.unwrap();
+    schedule_link(&document);
+}
+
 #[get("/api")]
-async fn schedule(mut req: web::Query<Request>) -> Result<impl Responder> {
+async fn schedule(req: web::Query<Request>) -> Result<impl Responder> {
     let preset = fs::read_to_string(PRESET)?;
     let spec: HashMap<String, Info> = ron::from_str(&preset)?;
 
-    //if !spec.contains_key(&req.spec) {
-    //
-    //}
-
-    if let Some(x) = spec.get(&req.spec) {
+    if let Some(info) = spec.get(&req.spec) {
+        do_schedule(info).await;
     } else {
         return Err(format!(
             "found `{}` expected one of: {:?}",
