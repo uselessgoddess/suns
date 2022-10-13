45d1ca37c08e7b249fe15d2b293e90547a1151d5
uselessgoddess <dodickgod@gmail.com>
2022-10-13 10:25:36 +0300

    Use less static types

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,52 @@
-mod faculties;
-
 use crate::faculties::Spec;
-use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder, Result};
+use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use futures::StreamExt;
+use ron::Value;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Info {
+    link: String,
+    row: usize,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Request {
+    spec: String,
     year: usize,
-    spec: Spec,
 }
+
+type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
+
+const PRESET: &str = "presets/preset.ron";
 
 #[get("/api")]
 async fn schedule(mut req: web::Query<Request>) -> Result<impl Responder> {
-    println!("{:?}", req);
+    let preset = fs::read_to_string(PRESET)?;
+    let spec: HashMap<String, Info> = ron::from_str(&preset)?;
+
+    //if !spec.contains_key(&req.spec) {
+    //
+    //}
+
+    if let Some(x) = spec.get(&req.spec) {
+    } else {
+        return Err(format!(
+            "found `{}` expected one of: {:?}",
+            req.spec,
+            spec.keys().collect::<Vec<_>>()
+        )
+        .into());
+    }
     Ok(HttpResponse::Ok())
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("http://localhost:8080/");
+
     HttpServer::new(|| App::new().service(schedule))
         .bind(("127.0.0.1", 8080))?
         .run()
